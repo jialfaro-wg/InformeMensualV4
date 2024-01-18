2bc0abff598b8121971a12d89e26065275005b11
Joaquín Iván Alfaro <jialfaro@workgroup.com.ar>
2024-01-18 10:22:13 -0300

    PrimerComitt

---
+++ InformeMensusalV4/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace InformeMensusalV4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dateTimePickerDesde = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerHasta = new System.Windows.Forms.DateTimePicker();
+            this.btnCargarExcel = new System.Windows.Forms.Button();
+            this.btnGenerarPDF = new System.Windows.Forms.Button();
+            this.dataGridViewTickets = new System.Windows.Forms.DataGridView();
+            this.webBrowserPDF = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTickets)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dateTimePickerDesde
+            // 
+            this.dateTimePickerDesde.Location = new System.Drawing.Point(12, 38);
+            this.dateTimePickerDesde.Name = "dateTimePickerDesde";
+            this.dateTimePickerDesde.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerDesde.TabIndex = 0;
+            // 
+            // dateTimePickerHasta
+            // 
+            this.dateTimePickerHasta.Location = new System.Drawing.Point(232, 38);
+            this.dateTimePickerHasta.Name = "dateTimePickerHasta";
+            this.dateTimePickerHasta.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerHasta.TabIndex = 1;
+            // 
+            // btnCargarExcel
+            // 
+            this.btnCargarExcel.Location = new System.Drawing.Point(508, 35);
+            this.btnCargarExcel.Name = "btnCargarExcel";
+            this.btnCargarExcel.Size = new System.Drawing.Size(75, 23);
+            this.btnCargarExcel.TabIndex = 2;
+            this.btnCargarExcel.Text = "Cargar Excel";
+            this.btnCargarExcel.UseVisualStyleBackColor = true;
+            this.btnCargarExcel.Click += new System.EventHandler(this.btnCargarExcel_Click);
+            // 
+            // btnGenerarPDF
+            // 
+            this.btnGenerarPDF.Location = new System.Drawing.Point(661, 35);
+            this.btnGenerarPDF.Name = "btnGenerarPDF";
+            this.btnGenerarPDF.Size = new System.Drawing.Size(95, 23);
+            this.btnGenerarPDF.TabIndex = 3;
+            this.btnGenerarPDF.Text = "Generar Informe";
+            this.btnGenerarPDF.UseVisualStyleBackColor = true;
+            this.btnGenerarPDF.Click += new System.EventHandler(this.btnGenerarPDF_Click);
+            // 
+            // dataGridViewTickets
+            // 
+            this.dataGridViewTickets.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTickets.Location = new System.Drawing.Point(55, 95);
+            this.dataGridViewTickets.Name = "dataGridViewTickets";
+            this.dataGridViewTickets.Size = new System.Drawing.Size(690, 250);
+            this.dataGridViewTickets.TabIndex = 4;
+            // 
+            // webBrowserPDF
+            // 
+            this.webBrowserPDF.Location = new System.Drawing.Point(55, 388);
+            this.webBrowserPDF.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserPDF.Name = "webBrowserPDF";
+            this.webBrowserPDF.Size = new System.Drawing.Size(690, 306);
+            this.webBrowserPDF.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Desde ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(324, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Hasta";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 742);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webBrowserPDF);
+            this.Controls.Add(this.dataGridViewTickets);
+            this.Controls.Add(this.btnGenerarPDF);
+            this.Controls.Add(this.btnCargarExcel);
+            this.Controls.Add(this.dateTimePickerHasta);
+            this.Controls.Add(this.dateTimePickerDesde);
+            this.Name = "Form1";
+            this.Text = "Informe Mensual";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTickets)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDesde;
+        private System.Windows.Forms.DateTimePicker dateTimePickerHasta;
+        private System.Windows.Forms.Button btnCargarExcel;
+        private System.Windows.Forms.Button btnGenerarPDF;
+        private System.Windows.Forms.DataGridView dataGridViewTickets;
+        private System.Windows.Forms.WebBrowser webBrowserPDF;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ InformeMensusalV4/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Drawing.Charts;
+using DocumentFormat.OpenXml.Office2013.Drawing.ChartStyle;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using DataTable = System.Data.DataTable;
+//using System.Data.DataTable = System.Data.System.Data.DataTable;
+
+namespace InformeMensusalV4
+{
+    public partial class Form1 : Form
+    {
+        //System.Data.DataTable data_Table; // Especifica el espacio de nombres para evitar la ambigüedad
+        DataTable data_Table;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCargarExcel_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Archivos Excel|*.xlsx";
+            openFileDialog1.Title = "Seleccionar archivo Excel";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                CargarDatosDesdeExcel(openFileDialog1.FileName);
+            }
+        }
+
+        private void CargarDatosDesdeExcel(string filePath)
+        {
+            try
+            {
+                using (var workbook = new XLWorkbook(filePath))
+                {
+                    var ws = workbook.Worksheets.First();
+                    //data_Table = ws.RangeUsed().AsTable().AsNativeSystem.Data.DataTable();
+                    data_Table = ws.RangeUsed().AsTable().AsNativeDataTable();
+
+
+                    dataGridViewTickets.DataSource = data_Table;
+
+                    // Aplicar el filtro por período de tiempo
+                    AplicarFiltroPorFecha();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al cargar el archivo Excel: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void AplicarFiltroPorFecha()
+        {
+            DateTime fechaDesde = dateTimePickerDesde.Value;
+            DateTime fechaHasta = dateTimePickerHasta.Value;
+
+            //data_Table.DefaultView.RowFilter = $"[Fecha-inicio] >= #{fechaDesde:MM/dd/yyyy}# AND [Fecha-cierre] <= #{fechaHasta:MM/dd/yyyy}#";
+            if (data_Table != null) // Verifica que data_Table no sea null
+            {
+                data_Table.DefaultView.RowFilter = $"[Fecha-inicio] >= #{fechaDesde:MM/dd/yyyy}# AND [Fecha-cierre] <= #{fechaHasta:MM/dd/yyyy}#";
+            }
+            else
+            {
+                MessageBox.Show("Error: El DataTable es nulo.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnGenerarPDF_Click(object sender, EventArgs e)
+        {
+            if (data_Table == null || data_Table.Rows.Count == 0)
+            {
+                MessageBox.Show("No hay datos para generar el PDF.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Lógica para generar el PDF
+            GenerarPDF();
+        }
+
+        private void GenerarPDF()
+        {
+            using (var document = new PdfDocument())
+            {
+                // Añadir página al PDF
+                PdfPage page = document.AddPage();
+
+                // Obtener el objeto XGraphics para dibujar en la página
+                using (var gfx = XGraphics.FromPdfPage(page))
+                {
+                    // Aquí puedes añadir el contenido al PDF, por ejemplo:
+                    XFont font = new XFont("Arial", 12);
+
+                    gfx.DrawString("INFOME MENSUAL", font, XBrushes.Black, new XPoint(30, 10));
+
+                    gfx.DrawString("------------------------------------", font, XBrushes.Black, new XPoint(30, 20));
+
+
+                    gfx.DrawString($"Cantidad de tickets totales: {data_Table.Rows.Count}", font, XBrushes.Black, new XPoint(30, 30));
+
+                    gfx.DrawString($"Cantidad de tickets en trámite: {ContarTicketsEnTramite()}", font, XBrushes.Black, new XPoint(30, 60));
+
+                    // Información de tickets por Web Service
+                    var ticketsPorWebService = ObtenerTicketsPorWebService();
+                    int posY = 90;
+
+                    foreach (var ws in ticketsPorWebService)
+                    {
+                        gfx.DrawString($"WebService: {ws.Key}", font, XBrushes.Black, new XPoint(30, posY));
+                        posY += 20;
+
+                        foreach (var tipo in ws.Value)
+                        {
+                            gfx.DrawString($"   Tipo: {tipo.Key}, Cantidad: {tipo.Value}", font, XBrushes.Black, new XPoint(30, posY));
+                            posY += 20;
+                        }
+                    }
+
+                    // Guardar el archivo PDF
+                    string pdfPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "InformeTickets.pdf");
+                    document.Save(pdfPath);
+
+                    // Mostrar el PDF en el WebBrowser
+                    webBrowserPDF.Url = new Uri(pdfPath);
+                }
+            }
+        }
+
+        private int ContarTicketsEnTramite()
+        {
+            if (data_Table == null || data_Table.Rows.Count == 0)
+                return 0;
+
+                int count = data_Table.AsEnumerable()
+                .Count(row => row.Field<string>("Estado") == "en proceso" || row.Field<string>("Estado") == "En Revisión" || row.Field<string>("Estado") == "abierto" || row.Field<string>("Estado") == "En curso" || row.Field<string>("Estado") == "En fila");
+
+            return count;
+        }
+
+
+        private Dictionary<string, Dictionary<string, int>> ObtenerTicketsPorWebService()
+        {
+            if (data_Table == null)
+            {
+                return new Dictionary<string, Dictionary<string, int>>(); // retorna un diccionario vacío si data_Table es null
+            }
+
+            // agrupo los tickets por Web Service y por tipo
+            var ticketsPorWebService = data_Table.AsEnumerable()
+                .Where(row => !row.IsNull("Name WS") && !row.IsNull("Tipo-TKT")) 
+                .GroupBy(row => row.Field<string>("Name WS"))
+                .ToDictionary(
+                    wsGroup => wsGroup.Key,  // nombre del Ws
+                    wsGroup => wsGroup
+                        .GroupBy(tipoGroup => tipoGroup.Field<string>("Tipo-TKT"))
+                        .ToDictionary(
+                            tipoGroup => tipoGroup.Key,  // tipo del Ticket
+                            tipoGroup => tipoGroup.Count()  // cntidad de Tickets por tipo
+                        )
+                );
+
+            return ticketsPorWebService;
+        }
+
+
+    }
+}
